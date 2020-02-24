9646e9ff06ce3e2fdecdaf21f5b6f59a9ef89023
Barbara Kudiess <bakudies@microsoft.com>
2020-02-24 15:02:36 -0800

    Wox UI project containing Xaml files referening view models from main wox project

---
+++ src/modules/wox/Wox.UI/ResultListBox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Runtime.Remoting.Contexts;
+
+namespace Wox.UI
+{
+    [Synchronization]
+    public partial class ResultListBox
+    {
+        public ResultListBox()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0 && e.AddedItems[0] != null)
+            {
+                ScrollIntoView(e.AddedItems[0]);
+            }
+        }
+
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            ((ListBoxItem)sender).IsSelected = true;
+        }
+    }
+}
