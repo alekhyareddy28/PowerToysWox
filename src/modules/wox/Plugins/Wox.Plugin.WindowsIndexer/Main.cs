d15d88b4b79b4e891f147024dbac7f37aae06d22
Alekhya Reddy Kommuru <reddykalekhya@gmail.com>
2020-03-03 18:03:24 -0800

    Added a sample code for the windows indexer plugin

--- src/modules/wox/Plugins/Wox.Plugin.WindowsIndexer/Main.cs
+++ src/modules/wox/Plugins/Wox.Plugin.WindowsIndexer/Main.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Wox.Plugin;
+using Wox.Infrastructure.Storage;
+using Wox.Plugin.WindowsIndexer;
 
 namespace Windows.Plugin.Indexer
 {
-    class Main : IPlugin
+    class Main : IPlugin, ISavable, IPluginI18n
     {
+
+        // This variable contains metadata about the Plugin
+        private PluginInitContext _context;
+
+        // This variable contains information about the context menus
+        private Settings _settings;
+
+        // Contains information about the plugin stored in json format
+        private PluginJsonStorage<Settings> _storage;
+
+        // To save the configurations of plugins
+        public void Save()
+        {
+            _storage.Save();
+        }
+
+        // This function uses the Windows indexer and returns the list of results obtained
         public List<Result> Query(Query query)
         {
             var result = new Result
@@ -22,7 +41,26 @@ namespace Windows.Plugin.Indexer
 
         public void Init(PluginInitContext context)
         {
-
+            // initialize the context of the plugin
+            _context = context;
+            _storage = new PluginJsonStorage<Settings>();
+            _settings = _storage.Load();
         }
+
+        // TODO: Localize the strings
+        // Set the Plugin Title
+        public string GetTranslatedPluginTitle()
+        {
+            return "Windows Indexer Plugin";
+        }
+
+        // TODO: Localize the string
+        // Set the plugin Description
+        public string GetTranslatedPluginDescription()
+        {
+            return "Returns files and folders";
+        }
+        
+
     }
 }
